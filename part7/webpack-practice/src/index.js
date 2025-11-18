4ba9443dda0764e9d25301ac5ad23a0178dc5317
Air M1 <airm1@MacBook-Air.local>
2025-11-18 16:37:59 +0100

    addded webpack config practice

---
+++ part7/webpack-practice/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import regeneratorRuntime from "regenerator-runtime";
+import "core-js/actual";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
